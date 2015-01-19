6aaab5b328cd70d9846f07edaeb510869911896e
lunchboxsuhi <morozoandrei@gmail.com>
2015-01-19 18:37:55 -0500

    AnnoucementController

---
+++ Hospice/Hospice/Controllers/AnnouncementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospice.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        // GET: Announcement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
