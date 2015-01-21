dd9f8e847bf7930cb91909a7e518d9aa09681b74
gee19 <gee19haine@gmail.com>
2015-01-20 20:35:14 -0500

    links & staff links done(add role to staff links after ryan is done)

--- Hospice/Hospice/Models/LinkStaff.cs
+++ Hospice/Hospice/Models/LinkStaff.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,25 @@ namespace Hospice.Models
             }
 
             public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "You cannot leave the name blank.")]
+        [StringLength(50, ErrorMessage = "Name cannot be more than 50 characters long.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Link")]
+        [Required(ErrorMessage = "You cannot leave the link blank.")]
+        [StringLength(1000, ErrorMessage = "Link cannot be more than 1000 characters long.")]
+        public string Link { get; set; }
+
+        [Display(Name = "isVisible")]
+        public bool isVisible { get; set; }
+        
+        //add role
+
+
+
+
 
         //public virtual ICollection<Users> Users { get; set; }
     }
