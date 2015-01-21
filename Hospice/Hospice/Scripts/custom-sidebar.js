75a3573ad13c03eb72fd58f611634d2ed674263f
lunchboxsuhi <morozoandrei@gmail.com>
2015-01-21 00:27:17 -0500

    Fixed a lot of Front End layout

--- Hospice/Hospice/Scripts/custom-sidebar.js
+++ Hospice/Hospice/Scripts/custom-sidebar.js
@@ -1,12 +1,24 @@
 ﻿//Change the active class on the sidebar depending on the page...
 $(document).ready(function () {
     //<![CDATA[
-    jQuery(function() {
-        jQuery('.sidebar ul li a').each(function() {
-            if (jQuery(this).attr('href')  ===  window.location.pathname) {
-                jQuery(this).addClass('active-app');
+    $(function() {
+        $('.sidebar ul li a').each(function() {
+            if ($(this).attr('href')  ===  window.location.pathname) {
+                $(this).addClass('active-app');
             }
         });
     });  
     //]]>
+
+
+
+    //make sidebar big as viewport or devicing viewing
+    var height = $(document).height();
+    $('.sidebar').height(height);
+
+
+    //Make the side Bar stick while scrolling
+    $(window).scroll(function () {
+        $('.sidebar').css(top, $(this).scrollTop());
+    });
 });
