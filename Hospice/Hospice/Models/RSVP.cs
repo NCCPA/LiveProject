2884ff4729391c9ee367af604c903dfb0e17094c
gee19 <gee19haine@gmail.com>
2015-01-20 20:09:48 -0500

    rsvp model done

--- Hospice/Hospice/Models/RSVP.cs
+++ Hospice/Hospice/Models/RSVP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +15,20 @@ namespace Hospice.Models
 
             public int ID { get; set; }
 
+            [Display(Name = "UserID")]
+            [Required(ErrorMessage = "You cannot leave the UserID blank.")]
+            [Range(1, 9999, ErrorMessage = "The number is not valid.")]
+            public int UserID { get; set; }
+
+            [Display(Name = "didRespond")]
+            public bool didRespond { get; set; }
+
+            [Display(Name = "isAttending")]
+            public bool isAttending { get; set; }
+
+            [Display(Name = "Note")]
+            [StringLength(1000, ErrorMessage = "The note cannot be more than 1000 characters long.")]
+            public string Note { get; set; }
         //public virtual ICollection<Users> Users { get; set; }
     }
 }
