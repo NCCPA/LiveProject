1a5c370b0552d3ecc3b802e4e0add31f241cf887
Ryan Morris <saveourmacs@gmail.com>
2015-02-01 23:57:19 -0500

    add announce

--- Hospice/Hospice/Models/User.cs
+++ Hospice/Hospice/Models/User.cs
@@ -25,13 +25,13 @@ namespace Hospice.Models
                 }
             }
 
-
+       
         [Display(Name="First Name")]
         [Required(ErrorMessage = "You cannot leave the first name blank.")]
         [StringLength(50, ErrorMessage = "First Name cannot be more than 50 characters long.")]
         public string FirstName { get; set; }
 
-
+     
         [Display(Name="Last Name")]
         [Required(ErrorMessage = "You cannot leave the last name blank.")]
         [StringLength(100, ErrorMessage = "Last Name cannot be more than 100 characters long.")]
